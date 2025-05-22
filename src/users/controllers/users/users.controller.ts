b03fbc124f93727bb7e8d5349bbc1461f80cdd00
jaybmuchiri <ibrahimainge@gmail.com>
2025-05-22 10:43:40 +0300

    feat: add more tables

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { CreateUserProfileDto } from 'src/users/dtos/CreateUserProfile.dto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
 import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
@@ -15,4 +17,26 @@ export class UsersController {
         createUser(@Body() createUserDto: CreateUserDto) {
             return this.userService.createUser(createUserDto)
         }
+
+        @Put(':id')
+        async updateUserById(
+        @Param('id', ParseIntPipe) id: number, 
+        @Body() updateUserDto: UpdateUserDto
+    ) {
+            await this.userService.updateUser(id, updateUserDto)
+        }
+
+        @Delete(':id')
+        async deleteUserById(
+            @Param('id', ParseIntPipe) id: number) {
+                await this.userService.deleteUser(id)
+            }
+
+        @Post(':id/profiles')
+        createUserProfile(
+            @Param('id', ParseIntPipe) id: number, 
+            @Body() createUserProfileDto: CreateUserProfileDto){
+        return this.userService.createUserProfile(id, createUserProfileDto);
+        }
+
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from 'src/typeorm/entities/Profile';
 import { User } from 'src/typeorm/entities/User';
-import { CreateUserParams } from 'src/utils/types';
+import { CreateUserParams, CreateUserProfileParams, UpdateUserParams } from 'src/utils/types';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
     constructor(
-        @InjectRepository(User) private userRepository: Repository<User>
+        @InjectRepository(User) private userRepository: Repository<User>,
+        @InjectRepository(Profile) private profileRepository: Repository<Profile>
     ) {}
 
     findUsers() {
-        return this.userRepository.find()
+        return this.userRepository.find({ relations: ['profile']})
     }
 
     createUser(userDetails: CreateUserParams) {
@@ -20,5 +22,27 @@ export class UsersService {
             createdAt: new Date(),
         })
         return this.userRepository.save(newUser);
+    }
+
+    updateUser(id:number, updateUserDetails: UpdateUserParams) {
+        return this.userRepository.update({ id }, {...updateUserDetails})
+    }
+    
+    deleteUser(id: number) {
+        return this.userRepository.delete({id});
+    }
+
+    async createUserProfile(
+        id: number, 
+        createUserProfile: CreateUserProfileParams
+    ) {
+        const user = await this.userRepository.findOneBy({ id });
+        if(!user)
+            throw new HttpException('User not found. Cannot create Profile', HttpStatus.BAD_REQUEST
+        );
+        const newProfile = this.profileRepository.create(createUserProfile);
+        const saveProfile = await this.profileRepository.save(newProfile);
+        user.profile = saveProfile;
+        return this.userRepository.save(user);
     }
 }
